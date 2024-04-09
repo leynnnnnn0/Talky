3e9843b68980c28b7019f02ed09f3c77ccd9f9b8
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-04-09 17:42:52 +0800

    finished splash screen

--- app/src/main/java/com/leynnnnnn/talky/SignUp.java
+++ app/src/main/java/com/leynnnnnn/talky/SignUp.java
@@ -1,7 +1,6 @@
 package com.leynnnnnn.talky;
 
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,14 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        new Handler().postDelayed(this::finish, 2000);
+        setContentView(R.layout.activity_sign_up);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
